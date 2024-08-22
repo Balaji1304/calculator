b86fc20902716092ac2ed6e9dcbc44d78bb23910
mr-black1304 <jr.musicclub@gmail.com>
2024-08-23 01:10:41 +0530

    Add ./xc.py

---
+++ xc.py
@@ -0,0 +1,10 @@
+
+proxi=int(input("proxi manual input"))
+twist=float(input("enter twist value"))
+tfo = int
+tfo = proxi*2
+spindle_speed = int
+spindle_speed=tfo/twist
+doffer_rpm = float 
+doffer_rpm=spindle_speed/9.42
+print(doffer_rpm)
