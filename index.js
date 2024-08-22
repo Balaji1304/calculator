6fb974c3586f36f09936c3d0035cdbc71cd9736a
mr-black1304 <jr.musicclub@gmail.com>
2024-08-23 00:38:22 +0530

    Add ./index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+
+
+function appendToDisplay(input){
+    const display = document.getElementById("display");
+    display.value += input; 
+    
+
+}
+
+function clearDisplay(){
+    display.value = "";
+
+}
+function calculate(){
+    try{
+    display.value = eval(display.value);
+    }
+    catch(error){
+    display.value = "error";
+    }
+}    
